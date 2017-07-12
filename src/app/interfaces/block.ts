70a3c49662771f6ddfdf6337ebcaebc4d9e0f7ab
mneucollins <mneucollins@gmail.com>
2017-07-11 22:08:29 -0400

    incremental -- beginning work on block view

--- src/app/interfaces/block.ts
+++ src/app/interfaces/block.ts
@@ -1,12 +1,12 @@
-import { IPanel } from './panel';
-import { IExhibit } from './exhibit';
-import { IFeature } from './feature';
+// import { IPanel } from './panel';
+// import { IExhibit } from './exhibit';
+// import { IFeature } from './feature';
 
 export interface IBlock {
     blockId: number;
     blockNum: string;
     imageUrl: string;
-    panels: IPanel[];
-    exhibits: IExhibit[];
-    blockFeatures: IFeature[];
+    // panels: IPanel[];
+    // exhibits: IExhibit[];
+    // blockFeatures: IFeature[];
 }
