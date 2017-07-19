8f175508db065a18b1cc77168ee7c22e97fef886
mneucollins <mneucollins@gmail.com>
2017-07-18 22:15:33 -0400

    roughs out block component ts needs template

--- src/app/the-quilt/block/block.component.ts
+++ src/app/the-quilt/block/block.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { BlockService } from '../../services/block.service';
+import { IBlock } from '../../interfaces/block';
 
 @Component({
   selector: 'app-block',
@@ -6,16 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [
     'block.component.scss',
     '../the-quilt-base.scss'
-  ]
+  ],
+  providers: [BlockService]
 })
 
 export class BlockComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  blocks:IBlock[];
+  errorMessage:string;
+  
+  constructor( private _router: Router, private _blockService: BlockService) { }
+  ngOnInit():void {
+    this._blockService.getBlocks()
+      .subscribe(blocks => this.blocks = blocks,
+          error => this.errorMessage = <any>error);    
   }
-
 }
-
-
